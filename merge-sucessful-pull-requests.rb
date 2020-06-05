c1f757a4dd2eead454da383589d9ff32e2de8712
Milica Cvrkota <cvrkotam@grinnell.edu>
2020-06-05 18:35:07 -0500

    explore: how to support auto merge

---
+++ merge-sucessful-pull-requests.rb
@@ -0,0 +1,28 @@
+# 1. We want to get all PRs from dependabot
+# 2. Iterate over those PRs that have been successfully tested
+# 3. We want to merge those PRs
+
+require "octokit"
+
+branch_name = ENV["BRANCH_NAME"]
+
+# Full name of the repo you want to create pull requests for.
+repo_name = ENV["PROJECT_PATH"] # namespace/project
+
+client = Octokit::Client.new(:login => 'x-access-token', :password => ENV["GITHUB_ACCESS_TOKEN"])
+
+pull_requests = client.pull_requests(ENV["PROJECT_PATH"], :state => 'open', :base => branch_name)
+
+puts pull_requests
+
+pull_requests.each do |pull|
+    puts "Checking status of #{pull.title}"
+    status = client.combined_status(repo_name, pull.head.ref)
+    puts "#{status.state}"
+    if status.state == "success" 
+        puts "Merging #{pull.title}"
+        client.merge_pull_request(repo_name, pull.number)
+    end
+
+    
+end
